792d5661ecff1e140094e8b21e652c4350652cce
Sidney François <sidney.fvm.dev@outlook.com>
2023-03-20 20:30:29 -0300

    add generator token

---
+++ Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace Blog
+{
+    public static class Configuration
+    {
+        // Token
+        public static string JWTKey { get; set; } = "eyJhbGciOiJIUzI1NiJ9eyJSb2xlIjoiQiLCJJc3N1ZXIi=";
+    }
+}
