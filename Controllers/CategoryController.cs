4934198cc1f284c70cca827583b72153684c15f1
Sidney François <sidney.fvm.dev@outlook.com>
2023-03-20 00:46:09 -0300

    add category controller

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,14 @@
+﻿using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet("categories")]
+        public IActionResult Get([FromServices] BlogDataContext context)
+        {
+            return Ok(context.Categories.ToList());
+        }
+    }
+}
